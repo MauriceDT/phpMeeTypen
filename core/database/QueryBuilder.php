742df350bb5aa84ee2a13e27b173512fcb5201aa
Maurice di Tolve <maurice_di_tolve@hotmail.com>
2020-12-16 16:07:19 +0100

    woensdag16dec_002

--- core/database/QueryBuilder.php
+++ core/database/QueryBuilder.php
@@ -22,17 +22,10 @@ class QueryBuilder
 
     public function insert($table, $parameters)
     {
-        // array_keys() returns an array containing the keys of the input array,
-        // and implode() concatenates each element of the new array into a
-        // string in which each element is separated by ", ":
         $columns = implode(', ', array_keys($parameters));
 
-        // Trick to preface each column name with a colon, which acts as a
-        // placeholder for the value corresponding to the specified key:
         $values = ':' . implode(', :', array_keys($parameters));
 
-        // sprintf() allows you to declare a string with placeholders to which
-        // you can attach variables (in this case, strings identified as %s):
         $sqlString = sprintf(
             'insert into %s (%s) values (%s)',
             $table,
@@ -43,8 +36,6 @@ class QueryBuilder
         try {
             $query = $this->pdo->prepare($sqlString);
 
-            // execute() accepts an array that is used in binding values to the
-            // placeholders (parameters) specified in $values:
             $query->execute($parameters);
         } catch (Exception $e) {
         }
